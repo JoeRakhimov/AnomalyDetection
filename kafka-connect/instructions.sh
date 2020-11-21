cd96fc14646eeedfe504d44e83bde6b1ca02cab5
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2020-11-21 02:26:19 +0100

    added Jupyter notebook for dataset modification and link for video instructions

--- kafka-connect/instructions.sh
+++ kafka-connect/instructions.sh
@@ -1,4 +1,5 @@
 # This file contains instructions to setup environment on PC
+# Same instruction in video format: https://youtu.be/PhDzCsgMvf0
 
 # A. 'kafka-connect' MODULE SETUP
 # 1. modify 'balance_hist_anon.csv': rename first column to 'id'
@@ -17,7 +18,7 @@ cd /Users/joe/kafka_2.13-2.6.0
 # 2. start server
 kafka-server-start config/server.properties
 
-# E. KAFKA
+# E. KAFKA CONNECT
 # 1. navigate to 'kafka-connect' folder
 cd /path/to/project/AnomalyDetection/kafka-connect
 # 2. create stream from csv file
